4e7b5d7ddc49881c3c7386fc05eace1a4e6f0c6a
zhouyangbin <zyb408770487@163.com>
2019-06-03 16:03:14 +0800

    1111

--- src/main/java/com/fairyland/mgr/test/T.java
+++ src/main/java/com/fairyland/mgr/test/T.java
@@ -15,7 +15,7 @@ public class T {
 	public static String show() {
 		System.out.println("ceshi github tijiao");
 		System.out.println("ceshi github tijiao2");
-		System.out.println("master版本,改一改改一改");		
+		System.out.println("master版本,我在dev上面做一些修改");		
 		int number1 = 1;
 		switch (number1) {
 		case 1:
